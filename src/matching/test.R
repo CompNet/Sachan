af03c29f2662cf7ddae4b12156916f02ea61eb9c
Vincent Labatut <vince.labatut@gmail.com>
2023-04-04 11:54:11 +0200

    updated R matching script

--- src/matching/test.R
+++ src/matching/test.R
@@ -5,20 +5,37 @@
 #
 # setwd("C:/Users/Vincent/eclipse/workspaces/Networks/Sachan")
 ###############################################################################
-
-
 library("igraph")
 library("iGraphMatch")
 
 
 
 
+###############################################################################
+# notes:
+# - comics:
+#   - last chapter: #143 (file cum/inst_143.graphml)
+#   - last scene: #1438 (file cum/inst_1437.graphml)
+# - tv show:
+#   - last S02 episode: #20 (file 19.graphml)
+#   - last S02 scene: #754 (file 753.graphml)
+#   - very last episode: #73 (file 72.graphml)
+#   - very last scene: #4165 (file 4164.graphml)
+# - novels:
+#   - last chapter:
+#   - last book 2 chapter:
+
 # read the episode-based tvshow static graph
-g.tv <- read.graph("in/tvshow/cumul/episode/72.graphml", format="graphml")
+g.tv <- read.graph("in/tvshow/cumul/scene/753.graphml", format="graphml")
+E(g.tv)$weight <- E(g.tv)$weight/max(E(g.tv)$weight)
 # read the scene-based comics static graph
 g.cx <- read.graph("in/comics/cumul/scene/cum_1437.graphml", format="graphml")
+E(g.cx)$weight <- E(g.cx)$Occurrences/max(E(g.cx)$Occurrences)
 
 
+
+
+###############################################################################
 res <- match_FW <- gm(
 	A=g.tv, B=g.cx,			# graphs to compare 
 	#seeds,					# known vertex matches
@@ -30,4 +47,4 @@ res <- match_FW <- gm(
 )
 
 matches <- cbind(res$corr_A, res$corr_B)
-print(cbind(V(g.tv)$id[res$corr_A], V(g.cx)$name[res$corr_B]))
+print(cbind(V(g.tv)$name[res$corr_A], V(g.cx)$name[res$corr_B]))
