31d06818a8c978b55eaecf56475ea3786ad169d0
Vincent Labatut <vince.labatut@gmail.com>
2023-04-06 10:30:24 +0200

    minor update in comics name normalization

--- src/preprocessing/name_normalization.R
+++ src/preprocessing/name_normalization.R
@@ -34,5 +34,7 @@ write.csv(x=names, file=tab.file, row.names=FALSE, fileEncoding="UTF-8")
 
 ###############################################################################
 # comics already have been normalized
-
-
+g.cx <- read.graph("in/comics/cumul/chapter/cum_143.graphml", format="graphml")
+names <- sort(V(g.cx)$name)
+tab.file <- "in/comics/charlist.csv"
+write.csv(x=names, file=tab.file, row.names=FALSE, fileEncoding="UTF-8")
