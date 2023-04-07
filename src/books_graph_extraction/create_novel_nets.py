89d1c37e221e4991ecd23b28ab3b4d98ee439228
Shaneul <73842546+Shaneul@users.noreply.github.com>
2023-04-07 14:18:45 +0100

    Create create_novel_nets.py

---
+++ src/books_graph_extraction/create_novel_nets.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  4 09:07:13 2023
+
+@author: shane
+"""
+
+import networkx as nx
+import pandas as pd
+from build_network import build_network, relabeled_with_id
+#nodesp[node].get(atr, default)
+if __name__ == '__main__':
+    
+    max_chapters = {'1.AGoT':72, '2.ACoK':69, '3.ASoS': 81, '4.AFFC': 45, '5.ADwD':72}
+    G_cumulative = nx.Graph()
+    for book in list(max_chapters.keys()):
+        for i in range(0, max_chapters[book] + 1):
+            G_instant = build_network(f'data/books/{book}',nx.Graph(),ch_start=i, ch_end=i)
+            G_cumulative = build_network(f'data/books/{book}',G_cumulative,ch_start=i, ch_end=i)
+            nx.set_node_attributes(G_instant, G_cumulative.nodes())
+            H_instant = relabeled_with_id(G_instant, 'name')
+            H_cumulative = relabeled_with_id(G_cumulative, 'name')
+            if i < 10:
+                nx.write_graphml(H_instant, f'data/books/nets/instant_chapter/{book}_0{i}_instant.graphml')
+                nx.write_graphml(H_cumulative, f'data/books/nets/cumulative_chapter/{book}_0{i}_cumul.graphml')
+            else:
+                nx.write_graphml(H_instant, f'data/books/nets/instant_chapter/{book}_{i}_instant.graphml')
+                nx.write_graphml(H_cumulative, f'data/books/nets/cumulative_chapter/{book}_{i}_cumul.graphml')                
+            
+        
