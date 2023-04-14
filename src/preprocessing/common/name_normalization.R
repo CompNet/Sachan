ef54dd03b11f509563ebe23f3d1ea0f677d97220
Vincent Labatut <vince.labatut@gmail.com>
2023-04-14 15:40:43 +0200

    slight updated of the main matrix

--- src/preprocessing/common/name_normalization.R
+++ src/preprocessing/common/name_normalization.R
@@ -16,6 +16,7 @@ library("igraph")
 # read the static tvshow graph
 g.tv <- read.graph("in/tvshow/cumul/episode/cumulative_72.graphml", format="graphml")
 names.tv <- sort(V(g.tv)$name)
+unnamed.tv <- 201	# counted manually in the Excel file
 #tab.file <- "in/tvshow/charlist.csv"
 #write.csv(x=names.tv, file=tab.file, row.names=FALSE, fileEncoding="UTF-8")
 
@@ -26,6 +27,7 @@ names.tv <- sort(V(g.tv)$name)
 # read the static novel graph
 g.nv <- read.graph("in/novels/cumul/5.ADwD_72_cumul.graphml", format="graphml")
 names.nv <- sort(V(g.nv)$name)
+unnamed.nv <- 52	# counted manually in the Excel file
 #tab.file <- "in/novels/charlist.csv"
 #write.csv(x=names.nv, file=tab.file, row.names=FALSE, fileEncoding="UTF-8")
 
@@ -36,6 +38,7 @@ names.nv <- sort(V(g.nv)$name)
 # comics already have been normalized
 g.cx <- read.graph("in/comics/cumul/chapter/cum_143.graphml", format="graphml")
 names.cx <- sort(V(g.cx)$name)
+unnamed.nv <- length(which(!V(g.cx)$Named))
 #tab.file <- "in/comics/charlist.csv"
 #write.csv(x=names.cx, file=tab.file, row.names=FALSE, fileEncoding="UTF-8")
 
@@ -54,6 +57,8 @@ stats["TV Show","TV Show"] <- length(names.tv)
 stats["TV Show","Comics"] <- length(intersect(names.tv,names.cx))
 stats["Comics","Comics"] <- length(names.cx)
 print(stats)
+cat("Characters present in all three narratives:",length(intersect(names.nv,intersect(names.tv,names.cx))),"\n")
+
 
 # compare with normalized list of characters
 char.file <- "in/characters.csv"
