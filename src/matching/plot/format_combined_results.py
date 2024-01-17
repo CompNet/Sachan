4cc816cc1c73beee8f56cedaa0909ee29aeeede2
Aethor <oow.autre@gmail.com>
2024-01-17 11:38:32 +0100

    narrative matching: table formatting enhancements

--- src/matching/plot/format_combined_results.py
+++ src/matching/plot/format_combined_results.py
@@ -5,7 +5,6 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 root_dir = f"{script_dir}/../../.."
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
@@ -29,13 +28,15 @@ if __name__ == "__main__":
         ) as f:
             dfs[medias] = pickle.load(f)
 
-    df = pd.DataFrame({medias: [df.max(axis=None)] for medias, df in dfs.items()})
+    df = pd.DataFrame({medias: [df["f1"].max(axis=None)] for medias, df in dfs.items()})
 
     if args.format == "plain":
         print(df)
         exit(0)
 
-    LaTeX_export = df.style.format(lambda v: "{:.2f}".format(v * 100)).to_latex(
-        hrules=True
+    LaTeX_export = (
+        df.style.format(lambda v: "{:.2f}".format(v * 100))
+        .hide(axis="index")
+        .to_latex(hrules=True)
     )
     print(LaTeX_export)
