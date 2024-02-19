4cab636cf8f9777b8f3748e1216804fdfe28f22b
Aethor <oow.autre@gmail.com>
2024-02-19 17:45:04 +0100

    narrative matching: update textual results table formatting

--- src/matching/plot/format_textual_results.py
+++ src/matching/plot/format_textual_results.py
@@ -1,4 +1,5 @@
 import argparse, os, pickle
+from posixpath import split
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 root_dir = f"{script_dir}/../../.."
@@ -10,13 +11,6 @@ if __name__ == "__main__":
     parser.add_argument(
         "-f", "--format", type=str, default="latex", help="either 'plain' or 'latex'"
     )
-    parser.add_argument(
-        "-a",
-        "--alignment",
-        type=str,
-        default="threshold",
-        help="one of 'threshold', 'smith-waterman'",
-    )
     args = parser.parse_args()
 
     dfs = {}
@@ -25,28 +19,29 @@ if __name__ == "__main__":
         with open(
             f"{root_dir}/out/matching/plot/{medias}_textual/df.pickle", "rb"
         ) as f:
-            df = pickle.load(f)
-            dfs[medias] = df[df["alignment"] == args.alignment]
+            dfs[medias] = pickle.load(f)
 
     df_dict = {
-        medias: df.loc[:, ["sim_fn", "f1"]].set_index("sim_fn")
-        for medias, df in dfs.items()
+        medias: df.loc[:, ["sim_fn", "alignment", "f1"]] for medias, df in dfs.items()
     }
 
     # output cols: sim_fn, f1_x, f1_y
     df = df_dict["tvshow-novels"].merge(
-        df_dict["tvshow-comics"], how="inner", on="sim_fn"
+        df_dict["tvshow-comics"], how="inner", on=["sim_fn", "alignment"]
     )
     # output cols: sim_fn, f1_x, f1_y, f1
-    df = df.merge(df_dict["comics-novels"], how="inner", on="sim_fn")
+    df = df.merge(df_dict["comics-novels"], how="inner", on=["sim_fn", "alignment"])
     df = df.rename(
         columns={
-            "f1_x": "tvshow-novels",
-            "f1_y": "tvshow-comics",
-            "f1": "comics-novels",
+            "sim_fn": "similarity",
+            "f1_x": "Novels vs. TV Show",
+            "f1_y": "Comics vs. TV Show",
+            "f1": "Novels vs. Comics",
         }
     )
-    df.index = df.index.rename("similarity")
+    df = df[["Novels vs. Comics", "Novels vs. TV Show", "Comics vs. TV Show"]]
+
+    df = df.set_index(["similarity", "alignment"])
 
     if args.format == "plain":
         print(df)
@@ -54,7 +49,15 @@ if __name__ == "__main__":
 
     LaTeX_export = (
         df.style.format(lambda v: "{:.2f}".format(v * 100))
+        .map_index(lambda v: "itshape: ;", axis="columns")
         .highlight_max(props="bfseries: ;", axis=0)
-        .to_latex(hrules=True)
+        .to_latex(hrules=True, column_format="llccc")
     )
+    # HACK: add a rogue midrule in between multirow blocks
+    # the header consists in \begin + \toprule + columns + index +
+    # midrule + first multirow (2 lines)
+    HEADER_SIZE = 7
+    splitted = LaTeX_export.split("\n")
+    splitted = splitted[:HEADER_SIZE] + ["\\midrule"] + splitted[HEADER_SIZE:]
+    LaTeX_export = "\n".join(splitted)
     print(LaTeX_export)
