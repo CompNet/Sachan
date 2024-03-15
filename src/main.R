15d0ed4b9eb9a76276dffe51d1b874f738a80c95
Vincent Labatut <vince.labatut@gmail.com>
2024-03-15 20:34:43 +0100

    completed main R script

--- src/main.R
+++ src/main.R
@@ -21,4 +21,38 @@ library("SDMTools")
 
 
 ###############################################################################
-# TODO
+source("src/visualization/narr_overlap.R")
+
+# plot static graphs
+source("src/visualization/static_plots.R")
+
+# plot top-20 graphs
+source("src/visualization/top20_plots.R")
+
+
+
+
+###############################################################################
+# topological properties of static graphs
+source("src/descript/static_props.R")
+
+# evolution of topological measures
+source("src/descript/evol_props.R")
+
+
+
+
+###############################################################################
+# graph matching
+source("src/vertex_matching/igm/exp_simple.R")
+source("src/vertex_matching/igm/exp_adaptive_hard_seeds.R")
+source("src/vertex_matching/igm/exp_adaptive_hard_seeds_temporal.R")
+source("src/vertex_matching/igm/exp_adaptive_soft_seeds.R")
+
+# jaccard sim matching
+source("src/vertex_matching/jaccard/descriptive_static.R")
+source("src/vertex_matching/jaccard/descriptive_dynamic.R")
+source("src/vertex_matching/jaccard/specific_chars.R")
+
+# centrality study
+source("src/descript/char_centrality.R")
